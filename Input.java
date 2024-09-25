d9cff41f1bbd94437d38e2549314f4f2b6f5a326
Greg Papadopoulos <venscarlet@MacBookAir.home>
2024-09-25 13:14:11 +0300

    Changed if to switch case

--- Input.java
+++ Input.java
@@ -2,35 +2,48 @@ public class Input extends Game {
 
     public void type(String location) {
         while (true) {
-            System.out.println("(Type 'inv' for inventory, 'quit' to exit, 'health' to show your HP.)");
+            System.out.println("(Type 'inv' for inventory, 'quit' to exit, 'health' to show your HP, 'use' and item name to use an item.)");
             String input = scanner.nextLine().toLowerCase();
+            String action = location + ":" + input;
 
-            
-            
-            if (input.equals("inv")) {
-                inventory.showInventory();
-            } else if (input.equals("quit")) {
-                System.out.println("Thanks for playing!");
-                command.waitForSeconds(2);
-                System.exit(0);
-            } else if (location.equals("center") && input.equals("north")) {
-                forest.north();
-                break;
-            } else if (location.equals("center") && input.equals("east")) {
-                forest.east();
-            } else if (location.equals("east forest") && input.equals("west")) {
-                forest.center();
-            } else if (location.equals("north forest") && input.equals("south")) {
-                forest.center();
-            } else if (location.equals("north forest") && input.equals("west")) {
-                System.out.println("The branches are too pointy. You lost 1 HP.");
-                left.loseLife(1);
-            } else if (input.equals("health")) {
-                left.isAlive();
-            } else if (input.contains("use")) {
-                System.out.println("Now's not the time for that.");
-            } else {
-                System.out.println("Unknown command.");
+            switch (action) {
+                case "center:north":
+                    forest.north();
+                    return; 
+                case "center:east":
+                    forest.east();
+                    break;
+                case "east forest:west":
+                    forest.center();
+                    break;
+                case "north forest:south":
+                    forest.center();
+                    break;
+                case "north forest:west":
+                    System.out.println("The branches are too pointy. You lost 1 HP.");
+                    left.loseLife(1);
+                    break;
+                default:
+                    switch (input) {
+                        case "inv":
+                            inventory.showInventory();
+                            break;
+                        case "quit":
+                            System.out.println("Thanks for playing!");
+                            command.waitForSeconds(2);
+                            System.exit(0);
+                            break;
+                        case "health":
+                            left.isAlive();
+                            break;
+                        default:
+                            if (input.contains("use")) {
+                                System.out.println("Now's not the time for that.");
+                            } else {
+                                System.out.println("Unknown command.");
+                            }
+                            break;
+                    }
             }
         }
     }
